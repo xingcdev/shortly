0ddaf5cb55a5c9995d62a21d12de6097a796f1fa
xingcdev <60323726+xingcdev@users.noreply.github.com>
2022-04-30 22:19:01 +0200

    working on hero section

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
 	mode: 'development',
 	devServer: {
 		static: {
-			directory: path.join(__dirname, './dist'),
+			directory: path.join(__dirname, './images'),
 		},
 		compress: true,
 		open: true,
@@ -44,6 +44,14 @@ module.exports = {
 					},
 				],
 			},
+			{
+				test: /\.(png|svg|jpg|jpeg|gif)$/i,
+				type: 'asset/resource',
+			},
+			{
+				test: /\.(html)$/,
+				use: ['html-loader']
+			}
 		],
 	},
 	resolve: {
