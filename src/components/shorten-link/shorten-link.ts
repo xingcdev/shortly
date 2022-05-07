d62eeead41f04123cb48ae7a2849b2e9ef200769
xingcdev <60323726+xingcdev@users.noreply.github.com>
2022-05-07 21:06:58 +0200

    add form validation

--- src/components/shorten-link/shorten-link.ts
+++ src/components/shorten-link/shorten-link.ts
@@ -2,19 +2,21 @@ import shortenLinkStyles from './shorten-link.styles.scss';
 
 class ShortenLink extends HTMLElement {
     template: HTMLTemplateElement;
+    errorMsg: HTMLParagraphElement;
+    shortenLinkInput: HTMLInputElement;
 
     constructor() {
         super();
 
         const shadowRoot = this.attachShadow({ mode: 'open' });
-
         this.template = document.createElement('template') as HTMLTemplateElement;
         this.template.innerHTML = `
                 <section class="shorten-link">
                     <section class="shorten-link__form">
                         <form>
                             <input class="shorten-link__input" type="text" name="shorten-link" placeholder="Shorten a link here..." required>
-                            <button class="btn--primary shorten-link__submit-button" type="submit">Shorten it!</button>
+                            <i><small class="shorten-link__js-error-msg"></small></i>
+                            <button class="btn--primary shorten-link__submit-btn" type="submit">Shorten it!</button>
                         </form>
                     </section>
     
@@ -41,7 +43,40 @@ class ShortenLink extends HTMLElement {
         //         .querySelector('.hamburger-nav__menu')
         //         .classList.toggle('hidden');
         // });
+        this.errorMsg = shadowRoot.querySelector(".shorten-link__js-error-msg")
+        this.shortenLinkInput = shadowRoot.querySelector(".shorten-link__input")
+        this.shortenLinkInput.addEventListener("input", this.handleInput)
+
+        shadowRoot.querySelector(".shorten-link__submit-btn").addEventListener("click", this.handleClick);
     }
+
+    handleClick = (e: Event) => {
+        e.preventDefault()
+        this.showError()
+    }
+
+
+    handleInput = (e:Event) => {
+        // Disable required HTML popup
+        e.preventDefault();
+        this.showError()
+    }
+
+    showError = () => {
+        if (this.shortenLinkInput.validity.valid) {
+            if (this.errorMsg.textContent) {
+                this.shortenLinkInput.classList.remove("error")
+                this.errorMsg.textContent = "";
+            }
+        } else {
+            if (!this.errorMsg.textContent) {
+                this.shortenLinkInput.classList.add("error")
+                this.errorMsg.textContent = "Please add a link";
+            }
+        }
+    }
+
+
 }
 
 window.customElements.define('shorten-link', ShortenLink);
